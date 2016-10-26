8661ede7448e1e26fa58bc1993d602e565fe6add
JosephKasprzyk <josephchristopherkasprzyk@gmail.com>
2016-10-26 13:42:33 -0500

    o8uygfo79f567

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HelloWorld
+
+Just a test
+
+CHANGELOG:
+
+added helloworld.py - 10/26/2016
+
---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# helloworld.py
+# Joseph Kasprztk
+# 10/26/2016
+
+"""Program that tells the world 'Hello'"""
+
+print("Hello, World!")
